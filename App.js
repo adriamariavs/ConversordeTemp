ccfac59e7798487b80f0811105039e2c83999e07
adriamariavs <125682807+adriamariavs@users.noreply.github.com>
2024-04-10 15:39:53 -0300

    10/04

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Modal } from "react-native";
+import React, { useState } from "react";
+import { styles } from "./src/style/Style";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  //Estado de valores
+  const [celsius, setCelsius] = useState("");
+  const [resultado, setResultado] = useState("");
+  const [modal, setModal] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  //Função de calculo
+  const conversao = () => {
+    const fahrenheit = (celsius * 9) / 5 + 32;
+
+    //Chamando resultado
+    setResultado(fahrenheit);
+    setModal(true);
+  };
+
+  // Const para fechar modal e apagar o valor
+  const calcularNovamente = () => {
+    setCelsius("");
+    setModal(false);
+  }
+
+    return (
+      <View style={styles.container}>
+        {/* View para caixa de fundo (css) */}
+        <View style={styles.caixa}>
+          {/* Titulo da pagina */}
+          <Text style={styles.titulo}> Conversão de Temperatura </Text>
+
+          {/* TextInput para colocar a valor para realizar o calculo */}
+          <TextInput
+            style={styles.TextInput}
+            placeholder="VALOR"
+            value={celsius}
+            onChangeText={setCelsius}
+          />
+
+          {/* Botão para calculo */}
+          <TouchableOpacity style={styles.Btn} onPress={conversao}>
+            {/* Titulo do botão */}
+            <Text style={styles.Txt}> CALCULAR </Text>
+          </TouchableOpacity>
+
+          {/* Texto de resultado */}
+          <Text style={styles.resultado}> {resultado} </Text>
+        </View>
+
+        {/* Abrir modal */}
+        <Modal visible={modal} onRequestClose={setModal}>
+          <View>
+            <TouchableOpacity
+              style={styles.BtnModal}
+              onPress={calcularNovamente}
+            >
+              {/* Titulo do botão */}
+              <Text style={styles.TxtModal}> CALCULAR NOVAMENTE </Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </View>
+    );
+}
---
+++ src/style/Style.js
@@ -0,0 +1,63 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#E0FFFF',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    
+    titulo: {
+     fontWeight: 'bold',
+     alignItems: 'center',
+     marginBottom: '5%',
+     fontSize: 30,
+     color: '#D8BFD8',
+    },
+
+    Btn: {
+     backgroundColor: '#D8BFD8',
+     alignItems: 'center',
+     borderRadius: 25,
+     width: '15%',
+     height: '5%',
+     justifyContent: 'center',
+    },
+
+    TextInput: {
+     alignItems: 'center',
+     justifyContent: 'center',
+     color: '#363636',
+     fontWeight: 'bold',
+     width: '25%',
+     height: '7%',
+     borderWidth: 1,
+     borderColor: '#D8BFD8',
+     marginBottom: '5%',
+     borderRadius: 25,
+     textAlign: 'center',
+    },
+
+    Txt: {
+     fontWeight: 'bold',
+     color: '#4F4F4F',
+    },
+
+    resultado: {
+     fontSize: 20,
+     fontWeight: 'bold',
+     marginTop: '5%',
+     color: '#4F4F4F',
+    },
+
+    caixa: {
+     backgroundColor: '#E6E6FA',
+     width: 1000,
+     height: 500,
+     alignItems: 'center',
+     justifyContent: 'center',
+     borderRadius: 25,
+    },
+
+  });
